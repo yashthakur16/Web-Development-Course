4c7386180021dcbd92093b3e2dbf8c8239ba9d45
yashthakur16 <tyash6843@gmail.com>
2024-09-10 09:38:23 +0530

    started with javascript done with variables, datatypes and operators

---
+++ JavaScript/lecture41/index.js
@@ -0,0 +1,65 @@
+// let a=10
+// let b=2
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a**b)
+
+// ternary operator
+
+// let age=18
+
+// let val=(age=>18)? 56:92
+// console.log(val)
+
+//Bitwise operator
+
+//AND
+
+console.log(5&6)
+
+//OR
+
+console.log(5|6)
+
+//NOT
+
+console.log(~5)
+
+//XOR
+
+console.log(2^2)
+
+//Left Shift
+
+console.log(5<<1)
+
+//Right Shift
+
+console.log(6>>1)
+
+//Switch Statement
+
+let day=2
+
+switch(day)
+{
+    case 1:console.log("Monday");
+    break;
+    case 2:console.log("Tuesday");
+    break;
+    case 3:console.log("Wednesday");
+    break;
+    case 4:console.log("Thursday");
+    break;
+    case 5:console.log("Friday");
+    break;
+    case 6:console.log("Saturday");
+    break;
+    case 7:console.log("Sunday");
+    break;
+    default:console.log("Enter valid number");
+}
